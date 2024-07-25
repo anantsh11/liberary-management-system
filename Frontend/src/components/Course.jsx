4c9628f32ec9ee7b2f9fc49afc8f4e24bce76d22
Akashssss <akashsingh43457@gmail.com>
2024-07-25 21:42:39 +0530

    add course page

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -20,19 +20,105 @@ function Course() {
     <>
       <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
         <div className="mt-28 items-center justify-center text-center">
-          <h1 className="text-2xl  md:text-4xl">
-            We're delighted to have you{" "}
-            <span className="text-pink-500"> Here! :)</span>
+          <h1 className="text-2xl  md:text-4xl mb-10">
+            
+            <span className="text-pink-500"> LATEST COURSES</span>
           </h1>
-          <p className="mt-12">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
-            assumenda? Repellendus, iste corrupti? Tempore laudantium
-            repellendus accusamus accusantium sed architecto odio, nisi expedita
-            quas quidem nesciunt debitis dolore non aspernatur praesentium
-            assumenda sint quibusdam, perspiciatis, explicabo sequi fugiat amet
-            animi eos aut. Nobis quisquam reiciendis sunt quis sed magnam
-            consequatur!
-          </p>
+          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div class=" m-4 relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-full ">
+              <div
+                class="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+                <img
+                  src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+                  alt="card-image" />
+              </div>
+              <div class="p-6">
+                <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                  React JS 
+                </h5>
+                <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit md:line-clamp-2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa at modi fugit delectus veritatis. Tempora.
+                </p>
+              </div>
+              <div class="p-6 pt-0">
+                <button
+                  class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+                  type="button">
+                  Read More
+                </button>
+              </div>
+
+            </div>  
+            <div class="m-4 relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-full  ">
+              <div
+                class="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+                <img
+                  src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+                  alt="card-image" />
+              </div>
+              <div class="p-6">
+                <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                  Next JS 
+                </h5>
+                <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit  md:line-clamp-2">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aut sit sed voluptatem soluta laboriosam quis unde illum corrupti odit.
+                </p>
+              </div>
+              <div class="p-6 pt-0">
+                <button
+                  class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+                  type="button">
+                  Read More
+                </button>
+              </div>
+            </div>  
+            <div class="m-4 relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-full ">
+              <div
+                class="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+                <img
+                  src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+                  alt="card-image" />
+              </div>
+              <div class="p-6">
+                <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                  ES6 Javascript
+                </h5>
+                <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit  md:line-clamp-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias libero deserunt vel, aperiam, laudantium repellendus deleniti necessitatibus quia laboriosam dignissimos pariatur obcaecati magni nulla est.
+                </p>
+              </div>
+              <div class="p-6 pt-0">
+                <button
+                  class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+                  type="button">
+                  Read More
+                </button>
+              </div>
+            </div>  
+            <div class=" m-4  relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-full ">
+              <div
+                class="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+                <img
+                  src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+                  alt="card-image" />
+              </div>
+              <div class="p-6">
+                <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                HTML & CSS
+                </h5>
+                <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit  md:line-clamp-2">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos temporibus unde soluta dolorem rerum vero consequuntur tempore?
+                </p>
+              </div>
+              <div class="p-6 pt-0">
+                <button
+                  class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+                  type="button">
+                  Read More
+                </button>
+              </div>
+            </div>  
+          </div>
           <Link to="/">
             <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
               Back
